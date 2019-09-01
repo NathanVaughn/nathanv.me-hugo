45cd18219cf93f62ed5321a50de7f4dc82e565bf
NathanVaughn <NathanVaughn@users.noreply.github.com>
2019-09-01 21:31:50 +0000

    Automated deployment: Sun Sep  1 21:31:50 UTC 2019 2f8339a68e562e75b74da2f5f844da384b1d14b4

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+// lazyloading config
+window.lazySizesConfig = window.lazySizesConfig || {};
+lazySizesConfig.preloadAfterLoad = false;
+
+document.addEventListener('DOMContentLoaded', function () {
+    // sidenav
+    elems = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elems);
+
+    // scrollspy for navbar
+    elems = document.querySelectorAll('.scrollspy');
+    options = {
+        scrollOffset: 75
+    };
+    M.ScrollSpy.init(elems, options);
+
+    // collpasible content
+    elems = document.querySelectorAll('.collapsible.popout');
+    M.Collapsible.init(elems);
+
+    // tabs for the projects
+    elems = document.querySelectorAll('.tabs');
+    M.Tabs.init(elems);
+
+    // mix it up
+    mixitup('.mix-container');
+
+    // project modals
+    elems = document.querySelectorAll('.modal');
+    M.Modal.init(elems);
+
+    // image lightboxes
+    elems = document.querySelectorAll('.materialboxed');
+    M.Materialbox.init(elems);
+
+    // tooltips for social media
+    elems = document.querySelectorAll('.tooltipped');
+    options = {
+        margin: 25
+    };
+    M.Tooltip.init(elems, options);
+});
